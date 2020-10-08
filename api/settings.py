be417763abebdb313ff3eaca5743e512238f4e0b
qundax <qunda.lim@outlook.sg>
2020-10-08 20:05:12 +0800

    modified public files

--- api/settings.py
+++ api/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
