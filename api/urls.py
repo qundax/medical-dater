75fb157ebb4f96711a2715df44fe1c5c87dee21a
qundax <qunda.lim@outlook.sg>
2020-10-09 09:32:10 +0800

    redirected loose links back to home

--- api/urls.py
+++ api/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('medical_devices.urls')),
+    path('<path:string>', TemplateView.as_view(template_name='index.html')),
 ]
