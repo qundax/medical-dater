ca98f320cd2decd79470b8132f8296dd658799a6
qundax <qunda.lim@outlook.sg>
2020-10-09 09:31:33 +0800

    added psql seed files

---
+++ db/seed.sql
@@ -0,0 +1,28 @@
+-- Main device types
+-- INSERT INTO medical_devices_category (name) VALUES ('All');
+-- INSERT INTO medical_devices_category (name) VALUES ('Urinary System');
+-- INSERT INTO medical_devices_category (name) VALUES ('Gastro-Intestinal Tract System');
+-- INSERT INTO medical_devices_category (name) VALUES ('Respiratory System');
+-- INSERT INTO medical_devices_category (name) VALUES ('Infusion Set / Adapter / Manometer / Atomizer');
+-- INSERT INTO medical_devices_category (name) VALUES ('Dressings');
+-- INSERT INTO medical_devices_category (name) VALUES ('Invasive Line / Catheter');
+
+
+-- Devices
+INSERT INTO medical_devices_medicaldevice (name, duration, image_src, category_id) VALUES ('100% Silicone Coated Latex Catheter', '7 days', 'https://res.cloudinary.com/dmzwdqyrd/image/upload/v1601001844/medical_dater/100_silicone_coated_latex_catheter_k5zayc.jpg', 2);
+INSERT INTO medical_devices_medicaldevice (name, duration, image_src, category_id) VALUES ('Urine Drainage Bag', '2 weeks', 'https://res.cloudinary.com/dmzwdqyrd/image/upload/v1601001846/medical_dater/urine_drainage_bag_hnpfjx.jpg', 2);
+
+INSERT INTO medical_devices_medicaldevice (name, duration, image_src, category_id) VALUES ('Nasogastric Tube (Polyethylene)', '7 days', 'https://res.cloudinary.com/dmzwdqyrd/image/upload/v1601001846/medical_dater/nasogastric_tube_lcxxte.jpg', 3);
+
+INSERT INTO medical_devices_medicaldevice (name, duration, image_src, category_id) VALUES ('Tracheostomy Tube', '1 months', 'https://res.cloudinary.com/dmzwdqyrd/image/upload/v1601001846/medical_dater/tracheostomy_tube_czmqfj.jpg', 4);
+INSERT INTO medical_devices_medicaldevice (name, duration, image_src, category_id) VALUES ('Endotracheal Tube', '7 days', 'https://res.cloudinary.com/dmzwdqyrd/image/upload/v1601001845/medical_dater/endotracheal_tube_l5smsn.jpg', 4);
+INSERT INTO medical_devices_medicaldevice (name, duration, image_src, category_id) VALUES ('Chest Tube Drainage Canister', '7 days', 'https://res.cloudinary.com/dmzwdqyrd/image/upload/v1601001844/medical_dater/chest_tube_drainage_canister_ejy60g.jpg', 4);
+INSERT INTO medical_devices_medicaldevice (name, duration, image_src, category_id) VALUES ('Chest Tube Drainage Bottle', '7 days', 'https://res.cloudinary.com/dmzwdqyrd/image/upload/v1601001801/medical_dater/chest_tube_drainage_bottle_rcvgjd.jpg', 4);
+
+INSERT INTO medical_devices_medicaldevice (name, duration, image_src, category_id) VALUES ('IV Drip Set', '3 days', 'https://res.cloudinary.com/dmzwdqyrd/image/upload/v1601001846/medical_dater/iv_strip_set_kque4j.jpg', 5);
+INSERT INTO medical_devices_medicaldevice (name, duration, image_src, category_id) VALUES ('IV Canula With Extension Tubing', '3 days', 'https://res.cloudinary.com/dmzwdqyrd/image/upload/v1601001845/medical_dater/iv_canula_w_extension_tubing_xvooqc.jpg', 5);
+INSERT INTO medical_devices_medicaldevice (name, duration, image_src, category_id) VALUES ('IV Burette', '10 days', 'https://res.cloudinary.com/dmzwdqyrd/image/upload/v1601001845/medical_dater/iv_burette_db8kg3.jpg', 5);
+
+INSERT INTO medical_devices_medicaldevice (name, duration, image_src, category_id) VALUES ('Central Venous Catheter (CVC) - Gauze', '48 hours', 'https://res.cloudinary.com/dmzwdqyrd/image/upload/v1601001847/medical_dater/cvc_dressing_ua4mym.jpg', 6);
+INSERT INTO medical_devices_medicaldevice (name, duration, image_src, category_id) VALUES ('Vacuum Assisted Closure Wound Drainage Canister', '7 days', 'https://res.cloudinary.com/dmzwdqyrd/image/upload/v1601001846/medical_dater/vacuum_assisted_closure_wound_drainage_canister_kqy0bb.jpg', 6);
+INSERT INTO medical_devices_medicaldevice (name, duration, image_src, category_id) VALUES ('Chest Tube Dressing', '48 hours', 'https://res.cloudinary.com/dmzwdqyrd/image/upload/v1601001845/medical_dater/chest_tube_dressing_tefm3h.jpg', 6);
